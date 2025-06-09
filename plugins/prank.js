681814bb5a2d0c71c7e246ce66bbe2bd9f480f82
dilawebx <lakshithadilmina73@gmail.com>
2025-06-09 15:42:48 +0100

    Update prank.js

--- plugins/prank.js
+++ plugins/prank.js
@@ -35,7 +35,7 @@ async (conn, mek, m, { from, quoted, reply }) => {
             '⚠️ _This is a simulated hacking activity for entertainment purposes._',
             '⚠️ _Remember: Ethical hacking ensures safety._',
             '',
-            '> *KAVIYA HUTHTHO: HACKING SIMULATION COMPLETE* ☣'
+            '> * SIMI: HACKING SIMULATION COMPLETE* ☣'
         ];
 
         for (const step of steps) {
